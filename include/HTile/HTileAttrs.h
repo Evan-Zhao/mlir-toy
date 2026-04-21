3ad8f5970149bcda2ba0235e321a53b5463b6040
Yifan Zhao <yifanz16@illinois.edu>
2026-04-21 13:08:07 -0500

    [WIP] Defining data placement attr in HTile

---
+++ include/HTile/HTileAttrs.h
@@ -0,0 +1,12 @@
+#ifndef HTILE_HTILEATTRS_H
+#define HTILE_HTILEATTRS_H
+
+#include "HTile/HTileDialect.h"
+#include "mlir/IR/Attributes.h"
+
+#include "HTileEnums.h.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "HTileAttrs.h.inc"
+
+#endif // HTILE_HTILEATTRS_H
--- lib/HTile/HTileDialect.cpp
+++ lib/HTile/HTileDialect.cpp
@@ -1,18 +1,31 @@
+#include "HTile/HTileAttrs.h"
 #include "HTile/HTileDialect.h"
 #include "HTile/HTileOps.h"
+#include "mlir/IR/DialectImplementation.h"
 #include "mlir/IR/OpImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
 
 #define GET_DIALECT_DEFS
 #include "HTileOpsDialect.cpp.inc"
 
 namespace htile {
 void HTileDialect::initialize() {
+  addAttributes<
+#define GET_ATTRDEF_LIST
+#include "HTileAttrs.cpp.inc"
+      >();
+
   addOperations<
 #define GET_OP_LIST
 #include "HTileOps.cpp.inc"
       >();
 }
 } // namespace htile
+
+#include "HTileEnums.cpp.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "HTileAttrs.cpp.inc"
 
 #define GET_OP_CLASSES
 #include "HTileOps.cpp.inc"
