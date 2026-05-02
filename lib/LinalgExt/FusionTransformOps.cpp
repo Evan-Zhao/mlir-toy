27ba9141cff84f62cff560036cb5f9e6dc88804d
Yifan Zhao <yifanz16@illinois.edu>
2026-05-02 06:11:01 -0500

    Split LinalgExtTransformOps.cpp into multiple files to make place for new ops

--- lib/LinalgExt/FusionTransformOps.cpp
+++ lib/LinalgExt/FusionTransformOps.cpp
@@ -1,16 +1,12 @@
 #include "LinalgExt/LinalgExtTransformOps.h"
-#include "LinalgExt/LinalgExtTransform.h"
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Arith/Utils/Utils.h"
 #include "mlir/Dialect/Linalg/IR/Linalg.h"
 #include "mlir/Dialect/SCF/IR/SCF.h"
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
-#include "mlir/Dialect/Transform/IR/TransformDialect.h"
 #include "mlir/Dialect/Transform/Interfaces/TransformInterfaces.h"
-#include "mlir/IR/DialectRegistry.h"
 #include "mlir/IR/IRMapping.h"
-#include "mlir/Tools/Plugins/DialectPlugin.h"
 #include "llvm/ADT/STLExtras.h"
 
 using namespace mlir;
@@ -533,34 +529,3 @@ DiagnosedSilenceableFailure LinalgExtFuseReductionConsumerIntoForallOp::apply(
 
 } // namespace transform
 } // namespace mlir
-
-namespace linalg_ext {
-
-void registerLinalgExtTransformExtension(mlir::DialectRegistry &registry) {
-  // MLIR 22.1.4 hangs in TransformDialectExtension::registerTransformOps for
-  // this out-of-tree plugin. Register the op directly as a narrow workaround;
-  // the op is still constrained by its TableGen traits and verified when used.
-  registry.addExtension(+[](mlir::MLIRContext *,
-                            mlir::transform::TransformDialect *dialect) {
-    struct TransformDialectAccess : public mlir::transform::TransformDialect {
-      using mlir::Dialect::addOperations;
-    };
-    static_cast<TransformDialectAccess *>(dialect)
-        ->addOperations<
-            mlir::transform::LinalgExtFuseMapConsumerIntoLoopOp,
-            mlir::transform::LinalgExtFuseReductionConsumerIntoForallOp>();
-  });
-}
-
-} // namespace linalg_ext
-
-#define GET_OP_CLASSES
-#include "LinalgExtTransformOps.cpp.inc"
-
-extern "C" LLVM_ATTRIBUTE_WEAK mlir::DialectPluginLibraryInfo
-mlirGetDialectPluginInfo() {
-  return {MLIR_PLUGIN_API_VERSION, "LinalgExtTransformPlugin",
-          LLVM_VERSION_STRING, [](mlir::DialectRegistry *registry) {
-            linalg_ext::registerLinalgExtTransformExtension(*registry);
-          }};
-}
