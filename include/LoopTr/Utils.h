b08746e3ae89e8bbe46f506d4f4b8569e3257850
Yifan Zhao <yifanz16@illinois.edu>
2026-05-10 14:51:54 -0500

    Util function to track for/forall output to in-loop producers

--- include/LoopTr/Utils.h
+++ include/LoopTr/Utils.h
@@ -1,7 +1,10 @@
 #ifndef LOOPTR_UTILS_H
 #define LOOPTR_UTILS_H
 
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Transform/Utils/DiagnosedSilenceableFailure.h"
+#include <variant>
 
 namespace mlir {
 
@@ -31,6 +34,29 @@ namespace mlir {
       return std::get<DiagnosedSilenceableFailure>(std::move(var##1));                             \
     (var) = std::get<OtherType>(var##1);                                                           \
   }
+
+/// Returns the unique tensor.parallel_insert_slice in `loop` that publishes `result`.
+FailureOr<tensor::ParallelInsertSliceOp> getParallelInsertSliceForLoopResult(scf::ForallOp loop,
+                                                                             OpResult result);
+
+using LoopResultMediator = std::variant<tensor::ParallelInsertSliceOp, tensor::InsertSliceOp>;
+
+struct LoopResultRelay {
+  OpResult inLoopResult;
+  OpResult loopReturnResult;
+  /// The operation that writes the loop-local result to the loop return tensor.
+  /// This is either a tensor.parallel_insert_slice, for an scf.forall loop,
+  /// or a (tensor.insert_slice or nullptr) for an scf.for loop.
+  Operation *mediator;
+};
+
+/// Returns loop-result relay information for each loop in `loops`, in the same order.
+/// `loops` must be nested outer-to-inner such that loops[i + 1]->getParentOp() == loops[i].
+/// The j-th entry of the i-th subvector describes the in-loop OpResult and mediator op that
+/// produce the j-th result of loops[i].
+FailureOr<SmallVector<SmallVector<LoopResultRelay>>>
+getNestedLoopResultRelays(ArrayRef<Operation *> loops);
+
 } // namespace mlir
 
 #endif // LOOPTR_UTILS_H
