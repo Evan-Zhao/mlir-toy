496507e5d8e3d9403ab1850a9d52cddc3443c044
Yifan Zhao <yifanz16@illinois.edu>
2026-05-20 12:11:22 -0500

    Avoid crash when Python interpreter fails (such as module not found)

--- lib/LoopTr/PythonSolver.cpp
+++ lib/LoopTr/PythonSolver.cpp
@@ -420,14 +420,16 @@ llvm::Expected<json::Value> solveRollingUpdaterWithPython(const json::Value &fEx
                                                           ArrayRef<std::string> rVariables,
                                                           StringRef accVar) {
   static std::once_flag initOnce;
-  static std::unique_ptr<py::scoped_interpreter> interpreter;
   static py::object solverModule;
   static py::object jsonModule;
   static std::string initError;
 
   std::call_once(initOnce, []() {
     try {
-      interpreter = std::make_unique<py::scoped_interpreter>();
+      // Intentionally leak the interpreter. If the d'tor of the interpreter throws an exception, it
+      // can cause a crash (likely during program shutdown, but still ugly).
+      auto interpreter = std::make_unique<py::scoped_interpreter>();
+      auto _ = interpreter.release();
       solverModule = py::module_::import("neptune_mlir.rolling_solver");
       jsonModule = py::module_::import("json");
     } catch (const py::error_already_set &e) {
