b8678d05057f15385c759f2138c370f720519833
Yifan Zhao <yifanz16@illinois.edu>
2026-07-25 16:11:47 -0500

    [HTile] htile.parallel_scatter provides a scatter op for the parallel region of scf.forall

--- include/HTile/HTileOps.h
+++ include/HTile/HTileOps.h
@@ -2,12 +2,14 @@
 #define HTILE_HTILEOPS_H
 
 #include "HTile/HTileDialect.h"
+#include "HTile/HTileAttrs.h"
 #include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/SymbolTable.h"
 #include "mlir/Interfaces/ControlFlowInterfaces.h"
+#include "mlir/Interfaces/ParallelCombiningOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
 
 #define GET_OP_CLASSES
